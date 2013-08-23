52fb20ca7f074d1f17426866da2469138a1605d2
Kaixhin <design@kaixhin.com>
2013-08-23 21:47:33 +0100

    Initial commit

---
+++ zk2hk.js
@@ -0,0 +1,17 @@
+﻿var hankaku = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~¢£¬¯¦¥₩';
+var zenkaku = '１２３４５６７８９０ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ　！＂＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠［＼］＾＿｀｛｜｝〜￠￡￢￣￤￥￦';
+
+var trim = function(word) {
+  return word.replace(/^\s+|\s+$/g, '');
+};
+
+var zenkakuToHankaku = function(word) {
+  for (var i = 0, n = zenkaku.length; i < n; i++) {
+    word = word.replace(new RegExp(zenkaku[i], 'gm'), hankaku[i]);
+  }
+  return word;
+};
+
+process.argv.splice(2).forEach(function (val) {
+  console.log(trim(zenkakuToHankaku(val)));
+});
